f6dabe42e65dea2fd9aacefd7661b9d82b35fcad
rohit adnaik <37871734+72ohit@users.noreply.github.com>
2022-01-27 11:09:19 +0530

    Update msforeachdatabase.sql

--- msforeachdatabase.sql
+++ msforeachdatabase.sql
@@ -5,3 +5,7 @@ DECLARE @command varchar(1000)
 SELECT @command = 'ALTER DATABASE ? SET COMPATIBILITY_LEVEL = 150' 
 
 EXEC sp_MSforeachdb @command 
+
+
+
+------https://www.mssqltips.com/sqlservertip/1414/run-same-command-on-all-sql-server-databases-without-cursors/
